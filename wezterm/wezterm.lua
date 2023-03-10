23690ad2eb64c5ddd2e9f6555db71f0627ebb65d
Cam Frey <camfrey@protonmail.com>
2023-03-09 23:36:47 -0500

    Add Hack NF as default for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,9 @@
+local wezterm = require 'wezterm'
+
 return {
   color_scheme = "tokyonight-storm",
+
+  -- Fonts
+  font = wezterm.font("MesloLGMDZ NFM", {weight="Regular", stretch="Normal", style="Normal"}),
 }
 
