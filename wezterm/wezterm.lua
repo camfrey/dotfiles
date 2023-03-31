72385cbc3151a21e29e6da98e6abe4f51dd69b7f
Cameron Frey <camfrey@protonmail.com>
2023-03-30 21:56:49 -0400

    Update to new wezterm config format

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,21 @@
+-- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
-return {
-  color_scheme = "tokyonight-storm",
+-- This table will hold the configuration.
+local config = {}
 
-  -- Fonts
-  font = wezterm.font("Hack Nerd Font", {weight="Regular", stretch="Normal", style="Normal"}),
-}
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Dark+'
+
+config.font = wezterm.font("Hack Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+
+-- and finally, return the configuration to wezterm
+return config
