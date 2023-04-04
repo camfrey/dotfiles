e59e3017b5ecfb528f3aaa255a8dd814698c22b0
Cameron Frey <camfrey@protonmail.com>
2023-04-03 21:20:33 -0400

    Remove script.sh that did nothing

--- script.sh
+++
@@ -1,3 +0,0 @@
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
