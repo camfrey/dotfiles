03b6e9b619304185f9fd306f4400aa0300ef6e45
Cameron Frey <dev@camfrey.io>
2024-01-29 18:02:11 -0500

    Add command line argument for install.ps1 to check wether to update rust config

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
+param([string]$r="no")
+
 $ErrorActionPreference = "Stop"
 
 $CONFIG_DIR="configs"
@@ -20,7 +22,9 @@ foreach ($PYTHON in ('python', 'python3')) {
         # Scoop
         &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c "$CONFIG_DIR/scoop.conf.yaml" -p "$PLUGINS_DIR/dotbot-scoop/scoop.py"
         # Rust
-        &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c "$CONFIG_DIR/rust_win.conf.yaml" -p "$PLUGINS_DIR/dotbot-rust/rust.py"
+        if($r -eq "yes") {
+          &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c "$CONFIG_DIR/rust_win.conf.yaml" -p "$PLUGINS_DIR/dotbot-rust/rust.py"
+        }
         # Linking
         &$PYTHON $(Join-Path $BASEDIR -ChildPath $DOTBOT_DIR | Join-Path -ChildPath $DOTBOT_BIN) -d $BASEDIR -c $CONFIG
         return
