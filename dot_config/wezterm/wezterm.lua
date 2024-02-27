b1cc1f1f1ca1faf3f61f2080f7a43c78bb61baea
Cam Frey <dev@camfrey.io>
2024-02-26 22:32:31 -0500

    Update helix and wezterm themes to github dark

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -13,7 +13,7 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Dark+'
+config.color_scheme = 'GitHub Dark'
 
 config.font = wezterm.font("Hack Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
 
