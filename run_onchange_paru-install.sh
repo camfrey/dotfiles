757dd9acdfcefae4648fd7d9845f8013a8ab0895
Cameron Frey <dev@camfrey.io>
2024-02-26 19:49:29 -0500

    Add paru instal script

---
+++ run_onchange_paru-install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Check if paru is installed
+if ! command -v paru &> /dev/null
+then
+  sudo pacman -S --needed base-devel
+  git clone --depth 1 https://aur.archlinux.org/paru.git
+  cd paru
+  makepkg -si
+  cd ..
+  rm -rf paru
+fi
+
+paru -S neovim cmake python ttf-hack-nerd
