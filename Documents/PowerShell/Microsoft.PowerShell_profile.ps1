ba6c4fae35767a9c76f1ff7512169728563bab17
Cam Frey <dev@camfrey.io>
2026-04-07 19:13:53 -0400

    Add some pwsh features

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,14 @@
 Invoke-Expression (&starship init powershell)
 
+# Enable history-based predictions
+Set-PSReadLineOption -PredictionSource History
+
+# Change the view to "ListView" for a more visual menu (press F2 to toggle)
+Set-PSReadLineOption -PredictionView ListView
+
+# Search history based on what you've already typed
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Use Ctrl+Space for a full menu-driven completion experience
+Set-PSReadLineKeyHandler -Chord "Ctrl+Spacebar" -Function MenuComplete
