ec0cdfc956cc1ee38105cfd68c126b672fad9cdc
Cam Frey <dev@camfrey.io>
2026-04-07 19:37:02 -0400

    Add some more pwsh completions

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,12 +3,32 @@ Invoke-Expression (&starship init powershell)
 # Enable history-based predictions
 Set-PSReadLineOption -PredictionSource History
 
-# Change the view to "ListView" for a more visual menu (press F2 to toggle)
-Set-PSReadLineOption -PredictionView ListView
-
 # Search history based on what you've already typed
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 # Use Ctrl+Space for a full menu-driven completion experience
 Set-PSReadLineKeyHandler -Chord "Ctrl+Spacebar" -Function MenuComplete
+
+# From PS Completions wiki
+# $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = [System.Text.UTF8Encoding]::new()
+
+$params = @{
+   # InlineView/ListView
+   PredictionViewStyle = 'ListView'
+   # Audible/Visual/None
+   BellStyle           = 'Audible'
+   # Windows/Emacs/Vi
+   EditMode            = 'Vi'
+}
+Set-PSReadLineOption @params
+Set-PSReadLineKeyHandler -Key 'Ctrl+z' -Function Undo
+
+Import-Module PSCompletions
+
+Import-Module CompletionPredictor
+
+$env:CARAPACE_TOOLTIP = 1
+carapace _carapace | Out-String | Invoke-Expression
+
+# $PSCompletions.argc_completions($argc_scripts)
